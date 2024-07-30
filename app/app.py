bdb76f6ce44e046c260f05bacdaef8e7a4d5037e
DanielC <daniel-castiblanco@outlook.com>
2024-07-30 11:07:16 -0400

    se realizan amplizaciones de codigo, falta correccion de codigo

--- app/app.py
+++ app/app.py
@@ -2,10 +2,41 @@
 
 #importamos las librerias
 
-from kivymd.app import MBApp
+from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDRaisedButton
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+#Definimos cada una de las pantallas
+
+class Inicio(Screen):
+    pass
+
+class Configuracion(Screen):
+    pass
+
+class Inventario(Screen):
+    pass
+
+#en este espacio se crea la aplicacion principal
+
+class MainApp(MDApp):
+    def build(self):
+        self.load_all_kv_files()
+        return Builder.load_file('main.kv')
+
+    def load_all_kv_files(self):
+        Builder.load_file('paginas/Inicio.kv')
+        Builder.load_file('paginas/Configuracion.kv')
+        Builder.load_file('paginas/Inventario.kv')
+
+    def on_menu_click(self):
+        print("al menu")
+
+#verificamos si se esta ejecutando como script principal
+
+if __name__ == "__main__":
+    MainApp().run()
 
