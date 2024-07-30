a9654c14da51a60fa7e4242497bad1c5636ed4d2
DanielC <daniel-castiblanco@outlook.com>
2024-07-29 20:44:31 -0400

    se escriben lineas de codigo sobre app.py main.kv y se crea una carpeta con la estructura de paginas

--- app/app.py
+++ app/app.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+#importamos las librerias
+
+from kivymd.app import MBApp
+from kivymd.uix.label import MDLabel
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
