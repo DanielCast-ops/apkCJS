28200112a708166815457ebd8d88f2dbed47325b
DanielC <daniel-castiblanco@outlook.com>
2024-07-31 10:27:02 -0400

    se corrigen bugs y se le prueva funcionamiento

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.toolbar import MDTopAppBar
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
@@ -23,24 +24,20 @@ class Inventario(Screen):
 #en este espacio se crea la aplicacion principal
 
 class MainApp(MDApp):
-    def construir(self):
-        self.load_all_kv_files()
-        return Builder.load_file('main.kv')
-
-    def cargar_documentos(self):
+    def build(self):
+        # Cargamos cada uno de los archivos .kv
         Builder.load_file('paginas/Inicio.kv')
         Builder.load_file('paginas/Configuracion.kv')
         Builder.load_file('paginas/Inventario.kv')
 
-    def dar_clic_menu(self):
-        print("al menu")
-
+        return Builder.load_file('main.kv')
 #aqui creo una funcion para el cambio de pantalla
 
     def cambiar_pantalla(self, nombre_pantalla):
         self.root.current = nombre_pantalla
 
-
+    def dar_clic_menu(self):
+        print("al menu")
 #verificamos si se esta ejecutando como script principal
 
 if __name__ == "__main__":
